8a9276483e28b7893c4e398c4e74c083d33fcbce
elysia <3302843873@qq.com>
2026-04-05 11:08:37 +0800

    Add Seele target support

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@
 #![cfg_attr(libc_deny_warnings, deny(warnings))]
 // Attributes needed when building as part of the standard library
 #![cfg_attr(feature = "rustc-dep-of-std", feature(link_cfg, no_core))]
+#![cfg_attr(target_os = "seele", allow(unused_features))]
 #![cfg_attr(feature = "rustc-dep-of-std", allow(internal_features))]
 // Some targets don't need `link_cfg` and emit a warning.
 #![cfg_attr(feature = "rustc-dep-of-std", allow(unused_features))]
--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -268,7 +268,7 @@ cfg_if! {
     }
 }
 cfg_if! {
-    if #[cfg(not(target_os = "redox"))] {
+    if #[cfg(not(any(target_os = "redox", target_os = "seele")))] {
         pub const FD_CLOEXEC: c_int = 0x1;
     }
 }
@@ -573,7 +573,7 @@ cfg_if! {
         #[link(name = "c")]
         #[link(name = "m")]
         extern "C" {}
-    } else if #[cfg(target_os = "redox")] {
+    } else if #[cfg(any(target_os = "redox", target_os = "seele"))] {
         #[cfg_attr(
             feature = "rustc-dep-of-std",
             link(
@@ -2206,7 +2206,7 @@ cfg_if! {
 }
 
 cfg_if! {
-    if #[cfg(not(target_os = "redox"))] {
+    if #[cfg(not(any(target_os = "redox", target_os = "seele")))] {
         extern "C" {
             pub fn getsid(pid: pid_t) -> pid_t;
             #[cfg_attr(
@@ -2489,7 +2489,7 @@ cfg_if! {
     } else if #[cfg(target_os = "haiku")] {
         mod haiku;
         pub use self::haiku::*;
-    } else if #[cfg(target_os = "redox")] {
+    } else if #[cfg(any(target_os = "redox", target_os = "seele"))] {
         mod redox;
         pub use self::redox::*;
     } else if #[cfg(target_os = "cygwin")] {
